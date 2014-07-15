479bcd84cd052125ee2cacab30b983a7f751d7ce
Nikhil Walvekar <walvekar@gmail.com>
2014-07-15 14:04:10 +0530

    Added support for preset shapes, transforms and colors.

--- PPTXSlideParser.js
+++ PPTXSlideParser.js
@@ -1,11 +1,27 @@
+function Shape() {
+	var type;
+	var textContent;
+}
+function Fill() {
+	var color;
+}
 function PPTXSlideParser(name, xmldoc, onSuccess, onFileReadError) {
 	var _api = {}
 	var slideinfo = {};
+	var shapes = [];
 	var xmlElementMap = {
 		'p:ph': parsePlaceHolder,
-		'a:t' : parseText
+		'p:sp': parseShape,
+		'a:prstGeom': parsePresetShape,
+		'a:t' : parseText,
+		'a:xfrm' : parseTransforms,
+		'a:off' : parseOffset,
+		'a:ext' : parseExtents,
+		'a:solidFill' : parseSolidFill,
+		'a:srgbClr' : parseSRGBColor
 	}
 	var lastShape = undefined;
+	var lastFill = undefined;
 	function _init() {
 		slideinfo['name'] = name;
 		parseFile();
@@ -14,6 +30,10 @@ function PPTXSlideParser(name, xmldoc, onSuccess, onFileReadError) {
 
 	function parseFile() {
 		parse(xmldoc.documentElement);
+		if (lastShape) {
+			shapes.push(lastShape);
+		}
+		slideinfo.shapes = shapes;
 	}
 
 	function parse(xmlnode) {
@@ -27,19 +47,60 @@ function PPTXSlideParser(name, xmldoc, onSuccess, onFileReadError) {
 			parse(children[child]);
 		}
 	}
+	function parsePresetShape(xmlnode) {
+		lastShape.type = 'presetShape';
+		lastShape.presetType = xmlnode.getAttribute('prst');
+	}
+
+	function parseShape(xmlnode) {
+		if (lastShape) {
+			shapes.push(lastShape);
+		}
+		lastShape = new Shape();
+	}
+	function parseTransforms(xmlnode) {
+		if (lastShape)
+			lastShape.xfrm = {};
+	}
+	function parseOffset(xmlnode) {
+		if (lastShape) {
+			var x = xmlnode.getAttribute('x');
+			var y = xmlnode.getAttribute('y');
+			lastShape.xfrm.off = {x : x, y: y}
+		}
+	}
+
+	function parseExtents(xmlnode) {
+		if (lastShape) {
+			var cx = xmlnode.getAttribute('cx');
+			var cy = xmlnode.getAttribute('cy');
+			lastShape.xfrm.ext = {cx : cx, cy: cy}
+		}
+	}
+
+	function parseSolidFill(xmlnode) {
+		lastFill = new Fill();
+		if (lastShape) {
+			lastShape.fill = lastFill;
+		}
+	}
+
+	function parseSRGBColor(xmlnode) {
+		lastFill.color = xmlnode.getAttribute('val');
+	}
 
 	function parsePlaceHolder(xmlnode) {
 		console.log(xmlnode.nodeName);
-		lastShape = xmlnode.getAttribute('type');
-		if (!lastShape) {
-			lastShape = 'content';
+		lastShape.type = xmlnode.getAttribute('type');
+		if (!(lastShape.type)) {
+			lastShape.type = 'content';
 		}
-		slideinfo[lastShape] = '';
+		lastShape.textContent = '';
 	}
 
 	function parseText(xmlnode) {
 		console.log(xmlnode.textContent);
-		slideinfo[lastShape] = slideinfo[lastShape] + xmlnode.textContent;
+		lastShape.textContent = lastShape.textContent + xmlnode.textContent;
 	}
 	_init();
 	return _api;
